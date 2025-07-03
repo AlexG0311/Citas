b2db625f3ede048bb171f2f4c0d2a2afb2962686
AlexG0311 <alexdavidgonzalez2003@gmail.com>
2025-07-02 19:19:58 -0500

    primer commit para mi rama

--- App.py
+++ App.py
@@ -19,5 +19,36 @@ def increment():
 def cliente_inicio():
     return render_template('ClienteInicio.html')
 
+
+@app.route('/AdminInicio')
+def admin_inicio():
+    return render_template('inicioAdmin.html')
+
+@app.route('/Servicios_Ofrecidos')
+def servicios_ofrecidos():
+    return render_template('servicios_ofrecidos.html')
+
+@app.route('/Registro_ personal')
+def registro_personal():
+    return render_template('registro_personal.html')
+
+@app.route('/Servicio_de_citas')
+def servicio_de_citas():
+    return render_template('servicio_de_citas.html')
+
+@app.route('/Horario_profesional')
+def horario_profesional():
+    return render_template('horario_profesional.html')
+
+@app.route('/Sedes')
+def sedes():
+    return render_template('sedes.html')
+
+@app.route('/Estadisticas')
+def estadisticas():
+    return render_template('estadisticas.html')
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
